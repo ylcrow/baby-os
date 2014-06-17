c409edd0ec73320ac0782fb3446408c85694eae4
ylcrow <ylcrow@gmail.com>
2014-06-17 13:58:11 +0800

    pm rm switch

--- orange/protect_mode/pmtest.asm
+++ orange/protect_mode/pmtest.asm
@@ -1,19 +1,19 @@
 %include  "pm.inc"
-org 07c00h
+
+org 0100h
 	jmp _code16_start
 
 
-
 [SECTION .gdt]
-;gdt
-desc_null:      Descriptor 0, 0, 0 
-desc_32code:    Descriptor 0, SEG_CODE32_LEN - 1, DA_C + DA_32
-desc_display:   Descriptor 0B8000h, 0ffffh,  DA_DRW
+;gdt            base addr --------  addr limit -------- desc attr
+desc_null:      Descriptor 0,       0,                  0               
+desc_32code:    Descriptor 0,       SEG_CODE32_LEN - 1, DA_C + DA_32    
+desc_display:   Descriptor 0B8000h, 0ffffh,             DA_DRW          
 
 
 ;gdt ptr
-gdt_ptr     dw      $ - desc_null - 1       ;word 16bits
-            dd      0                       ;double word 32bits
+gdt_ptr     dw      $ - desc_null - 1       ;16bits  gdt_limit (gdt_len - 1)
+            dd      0                       ;desc_null why?  32bits  gdt_baseaddr 
 
 
 ;selector
@@ -32,17 +32,18 @@ _code16_start:
     mov ss, ax
     mov sp, 0100h
 
+    ;fill desc_32code baseaddr 
     xor eax, eax   
     mov ax, cs
     shl eax, 4
     add eax, _code32_start
-
     mov word [desc_32code + 2], ax
     shr eax, 16
     mov byte [desc_32code + 4], al
     mov byte [desc_32code + 7], ah
 
 
+    ;load gdt
     xor eax, eax
     mov ax, cs
     shl eax, 4
@@ -51,9 +52,13 @@ _code16_start:
     lgdt [gdt_ptr]
 
 
+    ;关闭中断
     cli
 
-
+    ;open a20  
+    ;8086芯片在访问超过1M的的地址时会回滚
+    ;8086'OS为了向上兼容80286的芯片
+    ;在不修改系统代码的情况下，关掉20地址线就可以实现地址回滚
 	in	al, 92h
 	or	al, 00000010b
 	out	92h, al
