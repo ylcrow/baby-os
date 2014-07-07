47549d1b879da2d2bb695e120c78295dace94d48
xian.wang <ylcrow@gmail.com>
2014-07-07 23:21:24 +0800

    modify boot.asm

--- orange/loader/lookup_kernel/boot/boot.asm
+++ orange/loader/lookup_kernel/boot/boot.asm
@@ -1,20 +1,18 @@
-%include "def_addr.inc"
-
-
-org  BOOTIMG_OFFSET  
+    %include "def_addr.inc"
+    org  BOOTIMG_OFFSET  
     
     jmp short boot_start
     nop
 
     %include "fat12_hdr.asm" 
 
-    image_name      db	"LOADER  BIN"	    ; 需要加载的img名,fat12保存文件名时以两个space代替"."
-IMG_NAME_LEN    equ  $ - image_name         ; fat12: max 13 bytes(8文件名 + 3后缀名 + 2space)
+    image_name      db	"LOADER  BIN"	        ; 需要加载的img名,fat12保存文件名时以两个space代替"."
+    IMG_NAME_LEN    equ  $ - image_name         ; fat12: max 13 bytes(8文件名 + 3后缀名 + 2space)
 
-IMG_BASE        equ	LOADERIMG_VIRT_BASE_ADDR      	; image 被加载到的段地址
-IMG_OFFSET      equ	LOADERIMG_OFFSET                ; image 被加载到的偏移地址
+    IMG_BASE        equ	LOADERIMG_VIRT_BASE_ADDR      	; image 被加载到的段地址
+    IMG_OFFSET      equ	LOADERIMG_OFFSET                ; image 被加载到的偏移地址
 
-
+    
 boot_start:	
 	mov ax, cs
 	mov ds, ax
